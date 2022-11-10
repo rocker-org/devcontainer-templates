2192d91dc93990c656033e20311c9405e68c325e
eitsupi <50911393+eitsupi@users.noreply.github.com>
2022-11-11 00:42:50 +0900

    [CI] Swich the CI workflows to based on the `devcontainers/template-starter` template repo (#6)
    Copy the test workflow definition files from https://github.com/devcontainers/template-starter/tree/60daec8166ef58a812b0dd7297fc21b8cf974823

--- test/r-ver/test.sh
+++ test/r-ver/test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
 
-source test-utils.sh rstudio
+# shellcheck source=/dev/null
+source test-utils.sh
 
 checkCommon
 check "R" R --version
+check "radian" radian --version
 
 # Report result
 reportResults
