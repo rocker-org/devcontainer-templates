d400200ce791b94a519f797712c165cc0f2fa06b
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-01-09 01:03:44 +0900

    [r-ver] Remove the node Feature, add more comments, bump to version1.0.0 (#15)

--- test/r-ver/test.sh
+++ test/r-ver/test.sh
@@ -4,9 +4,15 @@ cd "$(dirname "$0")" || exit
 # shellcheck source=/dev/null
 source test-utils.sh
 
-checkCommon
 check "R" R --version
 check "radian" radian --version
+check "languageserver" R -q -e 'packageVersion("languageserver")'
+check "httpgd" R -q -e 'packageVersion("httpgd")'
+check "rmarkdown" R -q -e 'packageVersion("rmarkdown")'
+check "devtools" R -q -e 'packageVersion("devtools")'
+check "pak" R -q -e 'packageVersion("pak")'
+check "renv" R -q -e 'packageVersion("renv")'
+check "vscDebugger" R -q -e 'packageVersion("vscDebugger")'
 
 # Report result
 reportResults
