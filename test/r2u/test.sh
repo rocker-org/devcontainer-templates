602de29af660f600e48d09c16c2b4a136a0be6dc
eitsupi <50911393+eitsupi@users.noreply.github.com>
2023-08-06 11:07:33 +0000

    New template "r2u"

---
+++ test/r2u/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit
+
+# shellcheck source=/dev/null
+source test-utils.sh
+
+check "R" R --version
+check "radian" radian --version
+check "languageserver" R -q -e 'packageVersion("languageserver")'
+check "httpgd" R -q -e 'packageVersion("httpgd")'
+check "rmarkdown" R -q -e 'packageVersion("rmarkdown")'
+check "devtools" R -q -e 'packageVersion("devtools")'
+check "pak" R -q -e 'packageVersion("pak")'
+check "vscDebugger" R -q -e 'packageVersion("vscDebugger")'
+
+# Report result
+reportResults
